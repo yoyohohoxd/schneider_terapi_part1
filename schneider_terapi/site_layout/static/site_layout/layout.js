47953e55d48a94eef56c31bc899119157ade61d9
yoyohohoxd_WSL <madse@live.dk>
2024-02-11 18:00:24 +0100

    fixed header, burde virke nu

--- schneider_terapi/site_layout/static/site_layout/layout.js
+++ schneider_terapi/site_layout/static/site_layout/layout.js
@@ -1,4 +1,6 @@
 document.addEventListener('DOMContentLoaded', () => {
+    
+    
     const navEl = document.querySelector('.navbar');
 
     window.addEventListener('scroll', () => {
@@ -8,4 +10,24 @@ document.addEventListener('DOMContentLoaded', () => {
             navEl.classList.remove('navbar-scrolled');
         }
     });
+
+    const el_autohide = document.querySelector('.autohide');
+    if (el_autohide) {
+        console.log(el_autohide);
+    }
+
+    if(el_autohide) {
+        var last_scroll_top = 0;
+        window.addEventListener('scroll', () => {
+            let scroll_top = window.scrollY;
+            if(scroll_top < last_scroll_top) {
+                el_autohide.classList.remove('scrolled-down');
+                el_autohide.classList.add('scrolled-up');
+            } else {
+                el_autohide.classList.remove('scrolled-up');
+                el_autohide.classList.add('scrolled-down');
+            }
+            last_scroll_top = scroll_top;
+        });
+    }
 });
