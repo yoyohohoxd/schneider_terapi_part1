ccdce0a088db526f57ef2f0d79872982f96e6a8c
ChilliMads <madspagh@yahoo.dk>
2024-02-13 15:08:02 +0100

    Om_mig siden

--- schneider_terapi/site_layout/urls.py
+++ schneider_terapi/site_layout/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path("", views.index, name="index"),
     path('eft/', views.eft, name='eft'), # Mads: This is the view function for the EFT page
+    path('om_mig/', views.om_mig, name='om_mig'), # Mads: This is the view function for the om_mig page
 ]
