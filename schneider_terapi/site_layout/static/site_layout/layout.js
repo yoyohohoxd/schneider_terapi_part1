d3646c8bd2b375ec7164b83732104bad7bb7ea91
Mads-Emil Kjeldsen Hansen <madse@live.dk>
2024-02-28 20:22:57 +0100

    fixed navbar

--- schneider_terapi/site_layout/static/site_layout/layout.js
+++ schneider_terapi/site_layout/static/site_layout/layout.js
@@ -5,6 +5,8 @@ document.addEventListener('DOMContentLoaded', () => {
     const label = document.querySelector('[for=id_message]');
     const textArea = document.querySelector('.textarea');
 
+    console.log(navEl.offsetHeight);
+
     // Removes 'Besked' floating field when there is enough text
     // in the textArea that the window needs a scrollbar
     textArea.addEventListener('scroll', () => {
@@ -48,21 +50,4 @@ document.addEventListener('DOMContentLoaded', () => {
             navEl.classList.remove('navbar-scrolled');
         }
     });
-
-    const el_autohide = document.querySelector('.autohide');
-
-    if(el_autohide) {
-        var last_scroll_top = 0;
-        window.addEventListener('scroll', () => {
-            let scroll_top = window.scrollY;
-            if(scroll_top < last_scroll_top) {
-                el_autohide.classList.remove('scrolled-down');
-                el_autohide.classList.add('scrolled-up');
-            } else {
-                el_autohide.classList.remove('scrolled-up');
-                el_autohide.classList.add('scrolled-down');
-            }
-            last_scroll_top = scroll_top;
-        });
-    }
 });
