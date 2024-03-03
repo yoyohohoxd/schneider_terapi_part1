efc1014d4a28cd36a3e6acb10fd28e209b9cc5fb
ChilliMads <madspagh@yahoo.dk>
2024-03-03 19:57:23 +0100

    terapi

--- schneider_terapi/site_layout/urls.py
+++ schneider_terapi/site_layout/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path("", views.index, name="index"),
     path('eft/', views.eft, name='eft'), # Mads: This is the view function for the EFT page
     path('om_mig/', views.om_mig, name='om_mig'), # Mads: This is the view function for the om_mig page
-    path('priser_og_booking/', views.priser_og_booking, name="priser_og_booking")
+    path('priser_og_booking/', views.priser_og_booking, name="priser_og_booking"),
+    path('terapi/', views.terapi, name='terapi'),
 ]
